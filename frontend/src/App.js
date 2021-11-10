3d5228d871f7f938eac25be7a1e52174e4c58574
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-10 08:16:08 +0530

    remove extra code from master is come

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,15 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-
 // import {Button} from '@mui/material';
 import { Main } from "./components/HomePage/Main";
 import { Route, Routes } from "react-router-dom";
-import {Search} from './components/SearchPage/Search'
+import { Search } from "./components/SearchPage/Search";
 function App() {
   return (
-      <div className="App">
-        <Routes>
-          <Route path="/" exact element={<Main/>}/>
-          <Route path="/search" exact element={<Search/>}/>
-        </Routes>
-      </div>
-
->>>>>>> master
+    <div className="App">
+      <Routes>
+        <Route path="/" exact element={<Main />} />
+        <Route path="/search" exact element={<Search />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-=======
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -25,8 +11,6 @@ ReactDOM.render(
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
-
->>>>>>> master
 );
 
 // If you want to start measuring performance in your app, pass a function
