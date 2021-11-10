d0f804dfde30fd3244c9648a84661b79f5676d6a
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-11 03:59:42 +0530

    server file is done

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const connect = require("./config/db");
+
+require("dotenv").config();
+const app = express();
+const port = process.env.port;
+console.log("port:", port);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const start = async () => {
+  await connect();
+  app.listen(port, () => console.log(`app listening on port ${port}!`));
+};
+
+module.exports = start;
