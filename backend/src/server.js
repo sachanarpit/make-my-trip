bc9b2a036a2e06343e71f5cd09702fb21c43f696
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-11 15:18:45 +0530

    server file is exported

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const connect = require("./config/db");
 const app = express();
 require("dotenv").config();
 const port = process.env.port;
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const start = async () => {
+  await connect();
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+};
+
+module.exports = start;
