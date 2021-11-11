e559a58b514c22efea74ec3e9a8514ab9a7a4242
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-11 06:44:26 +0530

    checkout controller file post operation is ready

--- backend/src/controller/checkout.controller.js
+++ backend/src/controller/checkout.controller.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const rauter = express.Rauter();
 
-const Checkout = require("../models/checkout.model")
+const Checkout = require("../models/checkout.model");
+
+rauter.post("/", async (req, res) => {
+  const items = await Checkout.create(req.body);
+  return res.status(201).send(items);
+});
+
+module.exports = rauter;
