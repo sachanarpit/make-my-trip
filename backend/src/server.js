32d466928e708a52b964f53c46f562ff8f3339d0
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-11 16:18:20 +0530

    final push not working

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,13 @@ const connect = require("./config/db");
 const app = express();
 require("dotenv").config();
 const port = process.env.port;
+app.use(express.json());
+
+let userController = require("./controllers/user.controller");
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/users", userController);
 
 const start = async () => {
   await connect();
