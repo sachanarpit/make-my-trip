ed3facd991b875b21d7680d15a402013e86d9968
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-12 05:20:01 +0530

    user schma updated

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -5,6 +5,24 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  mobile: {
+    type: Number,
+    min: 1000000000,
+    max: 9999999999,
+    unique: true,
+  },
+  checkout: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "checkouts",
+    },
+  ],
+  order: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "orders",
+    },
+  ],
 });
 
 const User = mongoose.model("users", userSchema);
