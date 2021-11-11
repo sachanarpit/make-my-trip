db3014e665bc193938d56f5b5d44e30fe0d2e274
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-11 06:31:21 +0530

    simple schema for test is ready

---
+++ backend/src/models/checkout.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const checkoutSchema = new mongoose.Schema({
+    price: {
+        base_fare: { type: Number, required= true },
+        surcharges: { type: Number, required: true },
+        other: [{
+            type : Number
+        },]
+    },
+    date: { type: String },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "users"
+    }
+    
+});
+
