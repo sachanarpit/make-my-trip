c3ebcb284cba6a08806f949fdc7a7ed3786964ae
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-11 05:34:51 +0530

    user schema is written

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    id: { type: Number },
+    name: { type: String },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    mobile: {
+      type: Number,
+      min: 1000000000,
+      max: 9999999999,
+      unique: true,
+    },
+    cart: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: cart,
+      },
+    ],
+    checkout: [{ type: mongoose.Schema.Types.ObjectId, ref: checkout }],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
