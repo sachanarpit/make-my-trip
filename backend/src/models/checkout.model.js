d7916f7be5dd6b588fdeff8b343077a9b3574070
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-12 05:23:00 +0530

    checout model is ready

---
+++ backend/src/models/checkout.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const checkoutSchema = new mongoose.Schema({
+  price: {
+    base_fare: { type: Number, required: true },
+    surcharges: { type: Number, required: true },
+  },
+  date: { type: String },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+});
+
+const Checkout = mongoose.model("checkouts", checkoutSchema);
+
+module.exports = Checkout;
