49987857bc3f6c866c29b22a97c393886c8501b3
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-11 16:05:21 +0530

    user model is ready

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
