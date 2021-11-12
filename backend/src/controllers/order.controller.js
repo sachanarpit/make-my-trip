51acae9e931c08bbd9d9e72050e280eff1953fd1
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-12 08:05:20 +0530

    order controller is ready

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -2,4 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("" , )
+const Order = require("../models/order.model");
+
+router.post("", (req, res) => {
+  let user = await Order.create(req.body);
+  return res.send(user);
+});
+
+router.get("/:name", (req, res) => {
+  let regex = req.params.name;
+
+  Order.find({ user: regex }).then((result) => {
+    return res.status(200).json(result);
+  });
+});
+
+module.exports = router;
