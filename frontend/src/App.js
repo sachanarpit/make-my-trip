9d894a0274fd7dd70ecd2427bcffdb2a592eb1e9
Rahul yadav <rahul.yadav96@hotmail.com>
2021-11-12 23:12:56 +0530

    added userform

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,16 @@
 import { Main } from "./components/HomePage/Main";
 import { Route, Routes } from "react-router-dom";
 import { Search } from "./components/SearchPage/Search";
+import {Login} from "./components/login/Login";
 function App() {
+  // for popup 
+  const handlePopup = () => {
+    const popup = document.getElementById("popup");
+    popup.classList.toggle("active");
+  };
   return (
     <div className="App">
+      {/* <Login handleClick={handlePopup}/> */}
       <Routes>
         <Route path="/" exact element={<Main />} />
         <Route path="/search" exact element={<Search />} />
