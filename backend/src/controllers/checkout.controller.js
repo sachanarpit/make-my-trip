1ad06ef8dc682fd485f6c63f56fc937b0677182b
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-12 06:09:33 +0530

    api can't added more functionality

--- backend/src/controllers/checkout.controller.js
+++ backend/src/controllers/checkout.controller.js
@@ -3,9 +3,22 @@ const express = require("express");
 const rauter = express.Router();
 
 const Checkout = require("../models/checkout.model");
+const User = require("../models/user.model");
 
 rauter.post("/", async (req, res) => {
+  //   let data = req.body;
+  //   console.log(data.user);
+  //   //   let user = await User.findByIdAndUpdate(
+  //   //     data.user,
+  //   //     { $push: { checkout: { data } } },
+  //   //     { upsert: true, new: true }
+  //   //   );
+  //   let result = await User.findById(data.user);
+  //   result.checkout.push(data);
+  //   await result.save();
+
   const items = await Checkout.create(req.body);
+
   return res.status(201).send(items);
 });
 
