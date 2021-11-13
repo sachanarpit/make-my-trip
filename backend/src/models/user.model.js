5bd4214672d676e988b0966ff3f44ba19a7ca546
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-13 07:04:22 +0530

    user name is required

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  id: { type: String },
+  id: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
