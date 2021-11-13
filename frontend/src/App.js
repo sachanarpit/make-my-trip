5a285c395a667593b7a6333cf30cbd23e999837f
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-13 07:56:22 +0530

    payment code added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@
 import { Main } from "./components/HomePage/Main";
 import { Route, Routes } from "react-router-dom";
 import { Search } from "./components/SearchPage/Search";
+import { Payment } from "./components/paymentPage/Payment";
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" exact element={<Main />} />
         <Route path="/search" exact element={<Search />} />
+        <Route path="/payment" exact element={<Payment />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/components/paymentPage/Payment.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+import axios from "axios";
+
+function Payment() {
+  function loadScript(src) {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  }
+
+  async function displayRazorpay() {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+    }
+
+    const result = await axios.post("/payment/orders");
+
+    if (!result) {
+      alert("Server error. Are you online?");
+      return;
+    }
+
+    const { amount, id: order_id, currency } = result.data;
+
+    const options = {
+      key: "<YOUR RAZORPAY KEY>", // Enter the Key ID generated from the Dashboard
+      amount: amount.toString(),
+      currency: currency,
+      name: "Arpit Sachan",
+      description: "Test Transaction",
+      order_id: order_id,
+      handler: async function (response) {
+        const data = {
+          orderCreationId: order_id,
+          razorpayPaymentId: response.razorpay_payment_id,
+          razorpayOrderId: response.razorpay_order_id,
+          razorpaySignature: response.razorpay_signature,
+        };
+
+        const result = await axios.post("/payment/success", data);
+
+        alert(result.data.msg);
+      },
+      prefill: {
+        name: "<YOUR NAME>",
+        email: "example@example.com",
+        contact: "9999999999",
+      },
+      notes: {
+        address: "Example Corporate Office",
+      },
+      theme: {
+        color: "#61dafb",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Buy React now!</p>
+        <button className="App-link" onClick={displayRazorpay}>
+          Pay ₹500
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default Payment;
